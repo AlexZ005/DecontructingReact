20e79df550d0923017b9f539b1ffd38f3a5d9ee1
AlexZ005 <alexz005.info@gmail.com>
2020-03-14 22:14:05 +0200

    4:33 - 8:00 console logging elements

---
+++ index.tsx
@@ -0,0 +1,11 @@
+let React = {
+    createElement: (...args) => {
+    console.log(args);conso
+    },
+};
+
+const a = (<div className="react-2020">
+<h1>Hello, person!</h1>
+<p>Yes</p></div>
+);
+
